f6d3e8255d4d4f92e828da21df056f5740611e8f
Micah Lee <micah@micahflee.com>
2014-07-05 18:28:07 -0700

    added itsdangerous to setup-launcher.py for pyinstaller to find

--- setup/onionshare-launcher.py
+++ setup/onionshare-launcher.py
@@ -1,7 +1,7 @@
 # import stuff for pyinstaller to find
 import os, sys, subprocess, time, hashlib, platform, json, locale, socket, argparse, Queue, inspect, base64, random, functools
 import PyQt4.QtCore, PyQt4.QtGui, PyQt4.QtWebKit
-import stem, stem.control, flask
+import stem, stem.control, flask, itsdangerous
 import onionshare, onionshare_gui
 
 onionshare_gui.main()
